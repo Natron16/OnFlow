5b8c4e2cc8f61acfb522d6849024948fa7418339
Natron16 <144354043+Natron16@users.noreply.github.com>
2025-04-02 19:02:12 -0500

    Changes from pulling?

--- MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
+++ MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c16990ef746d7b3780c87fdd310d1b9833feac14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c492e7a83f027fde41eeeb65f6b898c5e8ff862a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
